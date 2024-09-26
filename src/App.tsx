63be32ce9ab1141002b9741c66f31d6f1d98ee3d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-09-26 15:45:15 +0700

    Init shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
+import { Charts } from "./Dashboard";
 import "./global.css";
-import styles1 from "./styles1.module.css";
-import styles2 from "./styles2.module.css";
 import DarkModeToggle from "./utils/DarkModeToggle";
 
 function App() {
   return (
     <main style={{ position: "relative" }}>
       <DarkModeToggle />
-      <h1>No Library, Just CSS</h1>
-      <article style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
-        <div className="card">
-          <i className="header">global</i>
-        </div>
-        <div className={`card ${styles1.card}`}>
-          <i className="header">style1</i>
-        </div>
-        <div className={`card ${styles2.card}`}>
-          <i className="header">style2</i>
-        </div>
-      </article>
+      <Charts />
     </main>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
+import path from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // https://github.com/vitejs/vite/discussions/5912#discussioncomment-2908994
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
