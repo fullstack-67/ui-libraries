c370bf646039d6ed15b75953c707b8e567a47466
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-09-27 21:37:19 +0700

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import DarkModeToggle from "./utils/DarkModeToggle";
 import "./global.css";
 import styles1 from "./styles1.module.css";
 import styles2 from "./styles2.module.css";
+import DarkModeToggle from "./utils/DarkModeToggle";
+
 function App() {
   return (
     <main style={{ position: "relative" }}>
       <DarkModeToggle />
       <h1>No Library, Just CSS</h1>
-
       <article style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
         <div className="card">
           <i className="header">global</i>
