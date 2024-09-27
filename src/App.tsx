08ecdd5b6827e4cab96fe4b7b839af34ad205c1b
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-09-27 11:02:37 +0700

    Init styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
+import styled from "styled-components";
+
 import "./global.css";
-import styles1 from "./styles1.module.css";
-import styles2 from "./styles2.module.css";
 import DarkModeToggle from "./utils/DarkModeToggle";
 
 function App() {
+  const Title = styled.h1`
+    color: var(--text);
+  `;
+
+  const Card = styled.div`
+    height: 20vh;
+    width: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 1rem;
+    background: var(--primary);
+    color: var(--text);
+  `;
+
   return (
     <main style={{ position: "relative" }}>
       <DarkModeToggle />
-      <h1>No Library, Just CSS</h1>
-      <article style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
-        <div className="card">
-          <i className="header">global</i>
-        </div>
-        <div className={`card ${styles1.card}`}>
-          <i className="header">style1</i>
-        </div>
-        <div className={`card ${styles2.card}`}>
-          <i className="header">style2</i>
-        </div>
-      </article>
+      <Title>Styled Component</Title>
+      <Card>
+        <i>Hello World!</i>
+      </Card>
     </main>
   );
 }
